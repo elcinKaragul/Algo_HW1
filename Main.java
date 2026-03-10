578e78e658be199b12fc43864bf67e13bfdad585
Elcin Karagul <elcinkaragul05@gmail.com>
2026-03-10 18:23:04 +0300

    maini yazdim

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
 package Algo_HW1;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		LinkedList list = new LinkedList(null, 0);
+
+		for (int i = 0; i < n; i++) {
+			int val = scanner.nextInt();
+			list.insert(val);
+		}
+
+
+
+
+		list.findEven(list.gethead());
+
+		list.printList();
+
+		scanner.close();
 
 	}
 
